f9f71b180bd19ec0b0c3345fae39885c86cc81b6
lukeisc123 <81511647+lukeisc123@users.noreply.github.com>
2024-11-28 21:31:47 +0800

    init

---
+++ app/src/main/java/com/example/httpserverproject_1155226712/ChatActivity.kt
@@ -0,0 +1,359 @@
+package com.example.httpserverproject_1155226712
+
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.unit.dp
+import com.example.httpserverproject_1155226712.ui.theme.HTTPServerProject_1155226712Theme
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+
+import androidx.compose.material.icons.Icons
+
+import androidx.compose.material.icons.filled.Refresh
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.runtime.mutableStateListOf
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class ChatActivity : ComponentActivity() {
+
+    data class Message(
+        val message: String,
+        val name: String,
+        val message_time: String,
+        val user_id: Int
+    )
+
+    val messages = mutableStateListOf<Message>()
+    private var chatroomId: Int = -1
+    private lateinit var chatroomName: String
+    private var userName: String = "ZHANG Xiangbo"
+    private var userId: Int = 1155226712
+
+    @SuppressLint("RememberReturnType")
+    @RequiresApi(Build.VERSION_CODES.O)
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        chatroomId = intent.getIntExtra("chatroomId", -1)
+        chatroomName = intent.getStringExtra("chatroomName").toString()
+
+        super.onCreate(savedInstanceState)
+        setContent {
+            HTTPServerProject_1155226712Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("$chatroomName")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = {
+                                    Intent(applicationContext, MainActivity::class.java).also {
+                                        startActivity(it)
+                                    }
+                                }
+                                )
+                                {
+                                    Icon(Icons.Filled.ArrowBack, null)
+                                }
+                            },
+
+                            actions = {
+                                IconButton(onClick = {
+                                    Intent(applicationContext, ChatActivity::class.java).also {
+                                        it.putExtra("chatroomId", chatroomId)
+                                        it.putExtra("chatroomName", chatroomName)
+                                        startActivity(it)
+                                    }
+
+                                }
+                                )
+                                {
+                                    Icon(Icons.Filled.Refresh, null)
+                                }
+                            }
+
+                        )
+                    },
+                    bottomBar = {
+                        BottomAppBar(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.primary,
+                        ) {
+                            var text by remember { mutableStateOf("") }
+                            Row(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Start
+                            )
+                            {
+                                TextField(
+                                    value = text,
+
+                                    onValueChange = {
+                                        text = it
+                                    },
+                                    label = { Text("Please enter your message") },
+                                    modifier = Modifier
+                                        .width(300.dp)
+                                )
+                                Button(
+                                    onClick = {
+                                        val currentTime = LocalDateTime.now()
+                                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+
+                                        val newMessageMap: Map<String, Any> = mapOf(
+                                            "message" to text,
+                                            "name" to userName,
+                                            "message_time" to currentTime,
+                                            "user_id" to userId,
+                                        )
+
+                                        if (text.isNotBlank()) {
+                                            CoroutineScope(Dispatchers.IO).launch {
+                                                val response = sendMessages(newMessageMap)
+                                                if (response == "OK") {
+                                                    messages.add(0,
+                                                        Message(
+                                                            message = newMessageMap["message"] as String,
+                                                            name = newMessageMap["name"] as String,
+                                                            message_time = newMessageMap["message_time"] as String,
+                                                            user_id = newMessageMap["user_id"] as Int,
+                                                        ))
+                                                    text = ""
+                                                }
+                                            }
+
+                                        }
+                                    },
+                                    modifier = Modifier.padding(8.dp)
+                                ) {
+                                    Text("Send")
+
+                                }
+
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(innerPadding)
+                                .align(Alignment.BottomStart),
+                            verticalArrangement = Arrangement.Bottom,
+
+                            horizontalAlignment = Alignment.Start
+
+                        ) {
+
+                            getMessagesCoro()
+
+                            LazyColumn(
+                                reverseLayout = true,
+                                verticalArrangement = Arrangement.Bottom
+                            ) {
+                                items(messages) { message ->
+                                    MessageCard((message),userName)
+                                }
+                            }
+
+
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
+
+    private fun getMessagesCoro() {
+        CoroutineScope(Dispatchers.Main).launch {
+            getMessages()
+        }
+    }
+
+    private suspend fun getMessages() {
+        withContext(Dispatchers.IO) {
+            val url = URL("http://iems5722-chatroomserverapp-1155226712.azurewebsites.net/get_messages/?chatroom_id=$chatroomId")
+            val connection = url.openConnection() as HttpURLConnection
+            try {
+                connection.requestMethod = "GET"
+                val response =
+                    BufferedReader(InputStreamReader(connection.inputStream)).use { it.readText() }
+                val json = JSONObject(response)
+                val data = json.getJSONObject("data").getJSONArray("messages")
+                messages.clear()
+                for (i in 0 until data.length()) {
+                    val messageObject = data.getJSONObject(i)
+                    val message = messageObject.getString("message")
+                    val name = messageObject.getString("name")
+                    val message_time = messageObject.getString("message_time")
+                    val user_id = messageObject.getInt("user_id")
+                    messages.add(Message(message, name, message_time, user_id))
+                }
+            } finally {
+                connection.disconnect()
+            }
+        }
+    }
+
+
+    private suspend fun sendMessages(newMessage: Map<String, Any>): String {
+        return withContext(Dispatchers.IO) {
+            try {
+                val newMessageWithChatroomId: Map<String, Any> = newMessage.toMutableMap().apply {
+                    put("chatroom_id", chatroomId)
+                }
+                val url = URL("http://iems5722-chatroomserverapp-1155226712.azurewebsites.net/send_message/")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
+
+                val jsonOutput = JSONObject(newMessageWithChatroomId).toString()
+                connection.outputStream.use { os ->
+                    val input = jsonOutput.toByteArray()
+                    os.write(input, 0, input.size)
+                }
+
+                val responseCode = connection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val responseBody = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonResponse = JSONObject(responseBody)
+                    return@withContext jsonResponse.getString("status")
+                } else {
+                    return@withContext "ERROR"
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return@withContext "ERROR"
+            }
+        }
+    }
+
+
+}
+
+@Composable
+@RequiresApi(Build.VERSION_CODES.O)
+fun MessageCard(msg: ChatActivity.Message, name: String) {
+    val alignmentMethod =
+        if (msg.name == name) Alignment.CenterEnd else Alignment.CenterStart
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = alignmentMethod
+
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(all = 8.dp)
+        ) {
+
+            Column(
+                horizontalAlignment = if (msg.name == name) Alignment.End else Alignment.Start
+            ) {
+                Text(
+                    text = msg.name,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    Text(
+                        text = msg.message,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+                val dateTime = LocalDateTime.parse(msg.message_time, formatter)
+
+                val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+                val newMessageTime = dateTime.format(timeFormatter)
+                Text(
+                    text = newMessageTime,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/httpserverproject_1155226712/MainActivity.kt
@@ -0,0 +1,321 @@
+package com.example.httpserverproject_1155226712
+
+import android.Manifest
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+
+import com.example.httpserverproject_1155226712.ui.theme.HTTPServerProject_1155226712Theme
+import com.google.firebase.messaging.Constants.TAG
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class MainActivity : ComponentActivity() {
+    private val chatrooms = mutableStateListOf<Chatroom>()
+    private val userId = 1155226712
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestNotificationPermission()
+        createNotificationChannel()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d(TAG, "FCM registration token: $token")
+
+            val newTokenMap: Map<String, Any> = mapOf(
+                "user_id" to userId,
+                "token" to token
+            )
+
+            // call the push function of token
+            CoroutineScope(Dispatchers.IO).launch {
+                pushTokens(newTokenMap)
+            }
+        }
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all").addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.e("TAG", "onCreate: subscribeToTopic")
+            } else {
+                Log.e("TAG", "onCreate: subscribeToTopic failed")
+            }
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            mainPage(chatrooms = chatrooms, applicationContext = this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    // mainpage function
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun mainPage(chatrooms: List<Chatroom>, applicationContext: Context) {
+
+        HTTPServerProject_1155226712Theme {
+            Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                TopAppBar(colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ), title = {
+                    Text("IEMS5722")
+                }, navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, null)
+                    }
+                })
+            }) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    getChatroomCoro()
+                    LazyColumn {
+                        items(chatrooms.reversed()) { chatroom ->
+                            OutlinedButton(
+                                onClick = {
+                                    Intent(applicationContext, ChatActivity::class.java).also {
+                                        it.putExtra("chatroomId", chatroom.id)
+                                        it.putExtra("chatroomName", chatroom.name)
+                                        startActivity(it)
+                                    }
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(2.dp),
+                                shape = RectangleShape
+
+                            ) {
+                                Text(
+                                    text = chatroom.name, fontSize = 20.sp
+                                )
+                            }
+                        }
+                    }
+
+
+                }
+            }
+
+        }
+    }
+
+    data class Chatroom(
+        val id: Int, val name: String
+    )
+
+    private suspend fun getChatroom() {
+        withContext(Dispatchers.IO) {
+            val url = URL("http://10.0.2.2:8086/get_chatrooms/")
+            val connection = url.openConnection() as HttpURLConnection
+            try {
+                connection.requestMethod = "GET"
+                val response =
+                    BufferedReader(InputStreamReader(connection.inputStream)).use { it.readText() }
+                // parse json
+                val json = JSONObject(response)
+                val data = json.getJSONArray("data")
+                chatrooms.clear()
+                for (i in 0 until data.length()) {
+                    val room = data.getJSONObject(i)
+                    chatrooms.add(Chatroom(room.getInt("id"), room.getString("name")))
+                }
+            } finally {
+                connection.disconnect()
+            }
+        }
+    }
+
+    private fun getChatroomCoro() {
+        CoroutineScope(Dispatchers.Main).launch {
+            getChatroom()
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val hasPermission = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (!hasPermission) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0
+                )
+            }
+            val channel = NotificationChannel(
+                "MyNotification", "MyNotification", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java) as NotificationManager
+            manager.createNotificationChannel(channel)
+
+
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val CHANNEL_ID = "my_channel_01"
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    // push tokens and user_id to server
+    private suspend fun pushTokens(newToken: Map<String, Any>): String {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("http://iems5722-chatroomserverapp-1155226712.azurewebsites.net/submit_push_token/")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
+                Log.d(
+                    "MainActivityLog",
+                    "Sending token: userId = ${newToken["user_id"]}, token = ${newToken["token"]}"
+                )
+                val jsonOutput = JSONObject(newToken).toString()
+                connection.outputStream.use { os ->
+                    val input = jsonOutput.toByteArray()
+                    os.write(input, 0, input.size)
+                }
+
+                val responseCode = connection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val responseBody = connection.inputStream.bufferedReader().use { it.readText() }
+                    val jsonResponse = JSONObject(responseBody)
+                    return@withContext jsonResponse.getString("status")
+                } else {
+                    return@withContext "ERROR"
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return@withContext "ERROR"
+            }
+        }
+    }
+}
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        remoteMessage.data.isNotEmpty().let {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.title, it.body)
+        }
+    }
+
+    private fun sendNotification(title: String?, body: String?) {
+        val CHANNEL_ID = "my_channel_01"
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.icon)
+            .setContentTitle(title).setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        val notificationId = 0
+        notificationManager.notify(notificationId, builder.build())
+
+    }
+
+}
