8b8fd6a4533e3c01ba5d3b4b086c07da2cb75c52
ljhOfGithub <1215161858@qq.com>
2024-12-01 22:20:12 +0800

    LoginActivity

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from fastapi import Request
 from pydantic import BaseModel
 from pymongo import MongoClient
 
+from sqlalchemy import create_engine, Column, Integer, String, Text, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 # for testing, you can update this one to your student ID
 student_list = [123456789, "123456789", 1155226712, "1155226712"]
 
@@ -21,13 +25,66 @@ COLLECTION_NAME1 = 'chatrooms'
 COLLECTION_NAME2 = 'messages'
 COLLECTION_NAME3 = 'tokens'
 
-ATLAS_URI = 'mongodb+srv://imzhangxiangbo:MTodRaDOvI0vJujY@cluster0.4donu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-atlas_client = MongoClient(ATLAS_URI)
-db = atlas_client[DB_NAME]
+# ATLAS_URI = 'mongodb+srv://imzhangxiangbo:MTodRaDOvI0vJujY@cluster0.4donu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+# atlas_client = MongoClient(ATLAS_URI)
+# db = atlas_client[DB_NAME]
 
-chatrooms_collection = db[COLLECTION_NAME1]
-messages_collection = db[COLLECTION_NAME2]
-tokens_collection = db[COLLECTION_NAME3]
+# chatrooms_collection = db[COLLECTION_NAME1]
+# messages_collection = db[COLLECTION_NAME2]
+# tokens_collection = db[COLLECTION_NAME3]
+
+# MySQL连接配置
+DATABASE_URL = "mysql+pymysql://root:root@localhost/android_project"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# 定义User类，对应数据库中的user表
+class User(Base):
+    __tablename__ = 'user'
+    # 定义id字段，对应数据库中的自增主键
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    # 定义account字段，对应数据库中的用户账号，设置为非空
+    account = Column(String(255), nullable=False)
+    # 定义password字段，对应数据库中的用户密码，设置为非空
+    password = Column(String(255), nullable=False)
+    # 定义nickname字段，对应数据库中的用户昵称，可为空
+    nickname = Column(String(255))
+    # 定义remark字段，对应数据库中的备注，类型为Text，可为空
+    remark = Column(String(255))
+    # 定义avatar字段，对应数据库中的头像（路径或URL），可为空
+    avatar = Column(String(255))
+    # 定义has_login字段，对应数据库中的登录状态，默认为False，类型为布尔型
+    has_login = Column(Boolean, default=False)
+
+# 请求体模型
+class UserLogin(BaseModel):
+    account: str
+    password: str
+
+# 登录和注册接口
+@app.post("/login")
+def login(user: UserLogin):
+    db = SessionLocal()
+    print(user)
+#     return 1
+    db_user = db.query(User).filter(User.account == user.account).first()
+
+    if db_user is None:
+        # 用户没有注册，自动注册
+        new_user = User(account=user.account, password=user.password, has_login=True)
+        db.add(new_user)
+        db.commit()
+        return {"message": "用户注册成功"}
+
+    # 用户已注册，验证密码
+    if db_user.password == user.password:
+        db_user.has_login = True
+        db.commit()
+        return {"message": "登录成功"}
+    else:
+        raise HTTPException(status_code=400, detail="密码错误")
 
 # get chatroom information
 @app.get("/get_chatrooms")
